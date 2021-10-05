401c756de7b0294cd08500e9cd72ce4c624a00cf
facade58 <78413837+facade58@users.noreply.github.com>
2021-10-05 18:31:53 +0530

    Add files via upload

--- Quiz.js
+++ Quiz.js
@@ -57,19 +57,19 @@ class Quiz {
           // fill("red");
         // }
 
-         //if (correctAns === allContestants[plr].answer){
-         //fill("Green")
-         //}
-        //else{
-          // fill("red");
-        // }
+         if (correctAns === allContestants[plr].answer){
+         fill("Green")
+      }
+        else{
+          fill("red");
+         }
 
-        if (correctAns === allContestants[plr].answer){
-           fill("red")
-         }
-         else{
-           fill("green");
-         }
+        //if (correctAns === allContestants[plr].answer){
+           //fill("red")
+        // }
+         //else{
+           //fill("green");
+         //}
 
         // if (correctAns = allContestants[plr].answer){
         //   fill("Green")
